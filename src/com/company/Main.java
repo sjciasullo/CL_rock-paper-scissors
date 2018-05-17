1a5291d51c420a4a85c00a994845375dcdaca640
Steven Ciasullo <sjciasullo@gmail.com>
2018-05-17 19:34:53 -0400

    add printWelcome method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        // TESTING AND NECESSARY COMMANDS FOR GAME RUNNING
         Player player1 = new Player("Player 1");
         Player player2 = new Player("Computer");
         System.out.println("player1's name is: " + player1.getName());
@@ -25,5 +26,19 @@ public class Main {
         gameState.addHistoryEntry("play1 wins play 2 loses");
         gameState.printHistory();
 
+        System.out.println();
+
+        // WILL GO IN WHILE LOOP UNTIL INPUT MESSAGE IS "quit"
+        printWelcome();
+
+    }
+
+    private static void printWelcome(){
+        System.out.println("Welcome to Rock-Paper-Scissors!\n");
+        System.out.println("MAIN MENU");
+        System.out.println("=========");
+        System.out.println("1. Type 'play' to play");
+        System.out.println("2. Type 'history' to view your game history");
+        System.out.println("3. Type 'quit' to stop playing");
     }
 }
