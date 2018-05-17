7ee163aa687036d627fc0b5d5dc0fee301ea76d2
Steven Ciasullo <sjciasullo@gmail.com>
2018-05-17 16:30:42 -0400

    create player class with public name access

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Player player1 = new Player("Player 1");
+        Player player2 = new Player("Computer");
+        System.out.println("player1's name is: " + player1.getName());
+        System.out.println("player2's name is: " + player2.getName());
     }
 }
